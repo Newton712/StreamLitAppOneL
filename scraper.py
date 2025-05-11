95158ad0f1b8b1439cd8f157012c0f51cda2ce15
Newton712 <cle710dropbox@gmail.com>
2025-05-11 18:08:18 +0200

    Update Chrome

--- scraper.py
+++ scraper.py
@@ -14,8 +14,12 @@ supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
 def start_browser():
     options = Options()
     options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--disable-gpu")
+    options.binary_location = "/usr/bin/google-chrome"
     return webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+
 
 def scrape_tournament_data(url):
     driver = start_browser()
